dafaf425f9aee8d3c89c50881bcccc9d00a19671
d-krava <kdmitriy535@gmail.com>
2026-03-16 10:21:18 +0100

    Update echo_server_socket.py

--- projet_tor_26/echo_server_socket.py
+++ projet_tor_26/echo_server_socket.py
@@ -1,1 +1,28 @@
+import socket
+import sys
 
+TCP_IP_srv = '10.102.74.44'
+TCP_PORT = 2000
+BUFFER_SIZE = 1024
+
+
+try :
+        tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error:
+        print('Une error')
+        sys.exit();
+
+tcp_socket.bind((TCP_IP_srv, TCP_PORT))
+
+tcp_socket.listen(3)
+print('En ecoute')
+
+connexion, adresse = tcp_socket.accept()
+print('Connecté avec : ', adresse)
+data = connexion.recv(BUFFER_SIZE)
+print('Message reçu du client : ', data)
+
+reponse_serveur = 'Merci pour la connexion'
+connexion.sendall(reponse_serveur.encode('utf8'))
+
+connexion.close()
